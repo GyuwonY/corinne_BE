4239e791f98dc0cd4c5ab205c729259431d464e9
Sungjin Kim <human9802@gmail.com>
2022-04-30 00:43:26 +0900

    start

---
+++ src/main/java/com/corinne/corinne_be/model/Quest.java
@@ -0,0 +1,28 @@
+package com.corinne.corinne_be.model;
+
+
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Table(name = "tbl_quest")
+@EntityListeners(AuditingEntityListener.class)
+public class Quest {
+
+    @GeneratedValue (strategy= GenerationType.IDENTITY)
+    @Id
+    @Column
+    private Long questId;
+
+    @Column (nullable = false)
+    private String title;
+
+    @Column (nullable = false)
+    private boolean clear;
+
+    @Column (nullable = false)
+    private int reword;
+}
