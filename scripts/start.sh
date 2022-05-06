fb94bd27429ee8981c52065161be8273f5cb5b16
DESKTOP-J4E816V\livia <ykw3480@gmail.com>
2022-05-07 00:02:21 +0900

    test

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,9 +5,9 @@ source ${ABSDIR}/profile.sh
 REPOSITORY=/home/ubuntu/sparta
 
 echo "> Build 파일 복사"
-echo "> cp $REPOSITORY/deploy/*.jar $REPOSITORY/"
+echo "> cp $REPOSITORY/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/deloy/*.jar $REPOSITORY/
+cp $REPOSITORY/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
