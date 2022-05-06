e622aa24bc5ba566e0461a0e6f13948b5bcd8236
DESKTOP-J4E816V\livia <ykw3480@gmail.com>
2022-05-06 22:21:25 +0900

    cd test

---
+++ scripts/start.sh
@@ -0,0 +1,30 @@
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
+
+REPOSITORY=/home/ec2-user/app
+PROJECT_NAME=springboot-intro
+
+echo "> Build 파일 복사"
+echo "> cp $REPOSITORY/deploy/*.jar $REPOSITORY/"
+
+cp $REPOSITORY/deloy/*.jar $REPOSITORY/
+
+echo "> 새 어플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+IDLE_PROFILE=$(find_idle_profile)
+
+echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
+
+nohup java -jar \
+    -Dspring.profiles.active=$IDLE_PROFILE \
+    -Djasypt.encryptor.password=$JASYPT_ENCRYPTOR_PASSWORD corinne_BE-0.0.1-SNAPSHOT.jar > nohup.out 2>&1 &
