94712c57f6d11a1135557694277e4b59c9977fb9
DESKTOP-J4E816V\livia <ykw3480@gmail.com>
2022-05-07 19:32:02 +0900

    test

--- scripts/start.sh
+++ scripts/start.sh
@@ -24,9 +24,8 @@ IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
-nohup java -jar \
-    -Dspring.config.location=classpath:/application.yml,classpath:/aws.yml,classpath:/application-$IDLE_PROFILE.properties \
-    -Dspring.profiles.active=$IDLE_PROFILE \
-    $REPOSITORY$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Dspring.config.location=classpath:/application.yml,classpath:/aws.yml,classpath:/application-$IDLE_PROFILE.properties -Dspring.profiles.active=$IDLE_PROFILE $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
+echo "nohup java -jar -Dspring.config.location=classpath:/application.yml,classpath:/aws.yml,classpath:/application-$IDLE_PROFILE.properties -Dspring.profiles.active=$IDLE_PROFILE $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &"
 
 exit 1
