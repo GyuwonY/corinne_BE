1d07e5aee64b2d35ef6e6cd7665fd93c8b209308
DESKTOP-J4E816V\livia <ykw3480@gmail.com>
2022-05-07 20:02:51 +0900

    test

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,10 +2,14 @@
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
+source ${ABSDIR}/password.sh
 REPOSITORY=/home/ubuntu/sparta/deploy
 
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/*.jar $REPOSITORY/"
+echo "${JASYPT_ENCRYPTOR_PASSWORD}"
+
+JASYPT_ENCRYPTOR_PASSWORD=${JASYPT_ENCRYPTOR_PASSWORD}
 
 cp $REPOSITORY/*.jar $REPOSITORY/
 
@@ -24,8 +28,8 @@ IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
-nohup java -jar -Dspring.config.location=classpath:/application.yml,classpath:/aws.yml,classpath:/application-$IDLE_PROFILE.properties -Dspring.profiles.active=$IDLE_PROFILE $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
-
-echo "nohup java -jar -Dspring.config.location=classpath:/application.yml,classpath:/aws.yml,classpath:/application-$IDLE_PROFILE.properties -Dspring.profiles.active=$IDLE_PROFILE $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &"
-
-exit 1
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.yml,classpath:/aws.yml,classpath:/application-$IDLE_PROFILE.properties \
+    -Dspring.profiles.active=$IDLE_PROFILE \
+    -Djasypt.encryptor.password=$JASYPT_ENCRYPTOR_PASSWORD \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
