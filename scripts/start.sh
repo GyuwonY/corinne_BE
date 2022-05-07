5db2f56811eaa9e65d1b8371ef91d15de498687b
DESKTOP-J4E816V\livia <ykw3480@gmail.com>
2022-05-07 22:05:56 +0900

    test

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,8 +7,6 @@ REPOSITORY=/home/ubuntu/sparta/deploy
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/*.jar $REPOSITORY/"
 
-echo "$JASYPT_ENCRYPTOR_PASSWORD"
-
 cp $REPOSITORY/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
@@ -27,6 +25,6 @@ IDLE_PROFILE=$(find_idle_profile)
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.yml,classpath:/aws.yml,classpath:/application-$IDLE_PROFILE.properties \
-    -Dspring.profiles.active=$IDLE_PROFILE \
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    -Dspring.config.location=classpath:/application.yml,classpath:/aws.yml,classpath:/application-${IDLE_PROFILE}.properties \
+    -Dspring.profiles.active=${IDLE_PROFILE} \
+    ${JAR_NAME} > ${REPOSITORY}/nohup.out 2>&1 &
