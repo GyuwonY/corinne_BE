84f68b45f85af49582c0635160ecd4dacfbcde5d
DESKTOP-J4E816V\livia <ykw3480@gmail.com>
2022-05-07 19:23:06 +0900

    test

--- scripts/start.sh
+++ scripts/start.sh
@@ -24,9 +24,7 @@ IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
-cd /home/ubuntu/sparta/deploy
-
 nohup java -jar \
     -Dspring.config.location=classpath:/application.yml,classpath:/aws.yml,classpath:/application-$IDLE_PROFILE.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    $REPOSITORY$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
