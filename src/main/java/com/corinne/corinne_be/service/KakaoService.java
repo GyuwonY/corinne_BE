fb5d31d2e6514fe27bdf980209c2bccfd8469390
DESKTOP-J4E816V\livia <ykw3480@gmail.com>
2022-05-13 23:16:35 +0900

    test 완료

--- src/main/java/com/corinne/corinne_be/service/KakaoService.java
+++ src/main/java/com/corinne/corinne_be/service/KakaoService.java
@@ -102,7 +102,7 @@ public class KakaoService {
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("grant_type", "authorization_code");
         body.add("client_id", "aa94123a5de4c285d81266d63666699f");
-        body.add("redirect_uri", "https://corinne-fe.vercel.app/user/kakao/callback");
+        body.add("redirect_uri", "http://localhost:3000");
         body.add("code", code);
 
 // HTTP 요청 보내기
