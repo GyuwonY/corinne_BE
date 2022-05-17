062f3c78dbbe9396bf810cb2a9618ec499b5163a
DESKTOP-J4E816V\livia <ykw3480@gmail.com>
2022-05-17 15:00:06 +0900

    알람 test

--- src/main/java/com/corinne/corinne_be/config/WebSockConfig.java
+++ src/main/java/com/corinne/corinne_be/config/WebSockConfig.java
@@ -1,9 +1,7 @@
 package com.corinne.corinne_be.config;
 
-import com.corinne.coin_data.websocket.handler.StompHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -13,7 +11,6 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
-    private final StompHandler stompHandler;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -28,8 +25,4 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
                 .withSockJS();
     }
 
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(stompHandler);
-    }
 }
