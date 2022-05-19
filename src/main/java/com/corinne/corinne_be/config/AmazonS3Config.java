f14dc8ddd26768fc590d37f2188201d691cde974
DESKTOP-J4E816V\livia <ykw3480@gmail.com>
2022-05-19 17:12:40 +0900

    test

--- src/main/java/com/corinne/corinne_be/config/AmazonS3Config.java
+++ src/main/java/com/corinne/corinne_be/config/AmazonS3Config.java
@@ -2,7 +2,7 @@ package com.corinne.corinne_be.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -20,9 +20,9 @@ public class AmazonS3Config {
     private String region;
 
     @Bean
-    public AmazonS3 s3Client() {
+    public AmazonS3Client amazonS3Client() {
         BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
-        return AmazonS3ClientBuilder.standard()
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
                 .build();
