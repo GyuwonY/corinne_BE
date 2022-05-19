6012cf66c01dece39ca191a9887ec69ab18aad36
DESKTOP-J4E816V\livia <ykw3480@gmail.com>
2022-05-19 16:21:07 +0900

    test

--- src/main/java/com/corinne/corinne_be/s3/StorageConfig.java
+++ src/main/java/com/corinne/corinne_be/s3/StorageConfig.java
@@ -9,23 +9,23 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class StorageConfig {
-
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String accessSecret;
-    @Value("${cloud.aws.region.static}")
-    private String region;
-
-    @Bean
-    public AmazonS3 s3Client() {
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, accessSecret);
-        return AmazonS3ClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(region).build();
-    }
-
-}
+//@Configuration
+//public class StorageConfig {
+//
+//    @Value("${cloud.aws.credentials.access-key}")
+//    private String accessKey;
+//
+//    @Value("${cloud.aws.credentials.secret-key}")
+//    private String accessSecret;
+//    @Value("${cloud.aws.region.static}")
+//    private String region;
+//
+//    @Bean
+//    public AmazonS3 s3Client() {
+//        AWSCredentials credentials = new BasicAWSCredentials(accessKey, accessSecret);
+//        return AmazonS3ClientBuilder.standard()
+//                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+//                .withRegion(region).build();
+//    }
+//
+//}
