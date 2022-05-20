b57554841973ece953bdb96a3118678439e0d0ad
DESKTOP-J4E816V\livia <ykw3480@gmail.com>
2022-05-20 18:10:11 +0900

    test

--- src/main/java/com/corinne/corinne_be/scheduler/WeeklyScheduler.java
+++ src/main/java/com/corinne/corinne_be/scheduler/WeeklyScheduler.java
@@ -52,7 +52,7 @@ public class WeeklyScheduler {
         this.questRepository = questRepository;
     }
 
-    @Scheduled(cron = "0 58 8 ? * FRI")
+    @Scheduled(cron = "0 14 9 ? * FRI")
     @Transactional
     public void rankUpdate() {
 
