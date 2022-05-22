9db7f5714672db35f7a10c930ea6478a4eef95c1
DESKTOP-J4E816V\livia <ykw3480@gmail.com>
2022-05-23 02:53:20 +0900

    최종 test

--- src/main/java/com/corinne/corinne_be/config/WebCORSConfig.java
+++ src/main/java/com/corinne/corinne_be/config/WebCORSConfig.java
@@ -12,7 +12,7 @@ public class WebCORSConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://corinne.kr")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .exposedHeaders(HttpHeaders.AUTHORIZATION)
