33a6a1b901efe0a97cb888335a5ba79dd9465cdc
DESKTOP-J4E816V\livia <ykw3480@gmail.com>
2022-05-22 22:26:56 +0900

    exception 수정

--- src/main/java/com/corinne/corinne_be/service/UserService.java
+++ src/main/java/com/corinne/corinne_be/service/UserService.java
@@ -71,7 +71,7 @@ public class UserService {
 
     //회원정보 조희
     private UserInfoResponesDto getUserInfo(Long userId){
-        User user = userRepository.findByUserId(userId).orElseThrow(IllegalArgumentException::new);
+        User user = userRepository.findByUserId(userId).orElseThrow(() -> new CustomException(ErrorCode.NON_EXIST_USER));
 
         Calendar cal = Calendar.getInstance();
         if(cal.get(Calendar.DAY_OF_WEEK)==1){
@@ -107,7 +107,7 @@ public class UserService {
     @Transactional
     public ProfileResponseDto registImage(MultipartFile file, UserDetailsImpl userDetails) throws IOException {
         Long userId = userDetails.getUser().getUserId();
-        User user = userRepository.findById(userId).orElseThrow(IllegalArgumentException::new);
+        User user = userRepository.findById(userId).orElseThrow(()-> new CustomException(ErrorCode.NON_EXIST_USER));
         String imgurl = s3Uploader.upload(file, "static");
         user.profileImgUpdate(imgurl);
         return new ProfileResponseDto(imgurl);
